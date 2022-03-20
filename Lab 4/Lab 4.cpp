086b9155dde706d7dba0af3a417c0f838796eac0
morko <morko@DANMERO>
2022-03-20 16:02:02 +0300

    Добавьте файлы проекта.

---
+++ Lab 4/Lab 4.cpp
@@ -0,0 +1,28 @@
+﻿#include <algorithm>
+
+#include <vector>
+
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    std::vector<int> v = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+    auto result = find_if(v.begin(), v.end(), [](int a)
+        {
+            int b;
+            cin >> b;
+            return a == b;
+          
+
+        });
+        if (result == v.end())
+            cout << "Number was not found!" << endl;
+        else
+            cout << "Number found at index : " << *result << endl;
+
+        return 0;
+}
